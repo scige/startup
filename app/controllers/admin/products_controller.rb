0f7458a7908592ccf68188ccc8904e142a77e8cf
scige <panf06@gmail.com>
2012-10-24 00:43:58 +0800

    prudcts new and edit finish

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,4 +2,30 @@ class Admin::ProductsController < ApplicationController
     def index
         @products = Product.all
     end
+
+    def new
+        @product = Product.new
+    end
+
+    def edit
+        @product = Product.find(params[:id])
+    end
+
+    def create
+        @product = Product.new(params[:product])
+        if @product.save
+            redirect_to :action => "index"
+        else
+            render :action => "new"
+        end
+    end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update_attributes(params[:product])
+            redirect_to :action => "index"
+        else
+            render :action => "edit"
+        end
+    end
 end
