6e7f7d24fc7ae8270e5e0a5bd44a217acec268e3
scige <panf06@gmail.com>
2012-10-30 22:25:27 +0800

    delete duplate code

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,2 +1,51 @@
 module Admin::ProductsHelper
+    def form_action(form_type)
+        if form_type == "edit"
+            return admin_product_path(@product)
+        else
+            return admin_products_path
+        end
+    end
+
+    def dashboard_select_products(dashboard_select)
+        if dashboard_select == "products"
+            return "current"
+        end
+            return ""
+    end
+
+    def dashboard_select_shops(dashboard_select)
+        if dashboard_select == "shops"
+            return "current"
+        end
+            return ""
+    end
+
+    def dashboard_select_users(dashboard_select)
+        if dashboard_select == "users"
+            return "current"
+        end
+            return ""
+    end
+
+    def dashboard_select_orders(dashboard_select)
+        if dashboard_select == "orders"
+            return "current"
+        end
+            return ""
+    end
+
+    def dashboard_select_categorys(dashboard_select)
+        if dashboard_select == "categorys"
+            return "current"
+        end
+            return ""
+    end
+
+    def dashboard_select_districts(dashboard_select)
+        if dashboard_select == "districts"
+            return "current"
+        end
+            return ""
+    end
 end
