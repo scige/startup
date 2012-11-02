b7c90d6bd063371660a4e9366c522dcaea277f0c
scige <panf06@gmail.com>
2012-11-02 23:56:41 +0800

    add home view and move view code

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Startup::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'products#index'
+  root :to => 'home#index'
   resources :products
 
   namespace :admin do
