f87d4519cedc59c5aaa29312bee6575bec88632e
scige <panf06@gmail.com>
2012-11-03 14:49:40 +0800

    finish admin::categories managerment

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,19 +1,38 @@
 class Admin::CategoriesController < ApplicationController
     def index
+        @categories = Category.all
     end
 
     def new
+        @category = Category.new
     end
 
     def edit
+        @category = Category.find(params[:id])
     end
 
     def create
+        @category = Category.new(params[:category])
+        if @category.save
+            redirect_to admin_categories_url
+        else
+            render :action => "new"
+        end
     end
 
     def update
+        @category = Category.find(params[:id])
+        if @category.update_attributes(params[:category])
+            redirect_to admin_categories_url
+        else
+            render :action => "edit"
+        end
     end
 
     def destroy
+        # TODO: 该分类下没有商品才能删除
+        @category = Category.find(params[:id])
+        @category.destroy
+        redirect_to admin_categories_url
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -52,17 +52,19 @@ Startup::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'home#index'
 
+  resources :products
+
   resources :categories do
     collection do
         get :today
     end
   end
 
-  resources :products
 
   namespace :admin do
     root :to => 'products#index'
     resources :products
+    resources :categories
   end
 
   # See how all your routes lay out with "rake routes"
