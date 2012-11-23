3c8562246701cfc8172bc776e4dda943e14825bc
scige <panf06@gmail.com>
2012-11-23 23:59:07 +0800

    add title for all pages

--- app/helpers/filters_helper.rb
+++ app/helpers/filters_helper.rb
@@ -1,2 +1,9 @@
 module FiltersHelper
+    def filter_title(action_name, id)
+        if action_name == "category"
+            Category.find(id.to_i).cn_name
+        elsif action_name == "district"
+            District.find(id.to_i).cn_name
+        end
+    end
 end
