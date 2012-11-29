d3de94100bf3697f7594ffe982051e13923af2a1
scige <panf06@gmail.com>
2012-11-30 00:08:23 +0800

    compute map center

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,13 @@
 module HomeHelper
+    def compute_map_center_x(products)
+        sum = 0
+        products.each {|p| sum += p.pos_x}
+        sum / products.size
+    end
+
+    def compute_map_center_y(products)
+        sum = 0
+        products.each {|p| sum += p.pos_y}
+        sum / products.size
+    end
 end
