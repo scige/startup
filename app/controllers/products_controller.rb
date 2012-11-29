549ac6b2bdeaafec512150c0cac198d70cc367d6
scige <panf06@gmail.com>
2012-11-29 23:24:26 +0800

    finish difficult feature: add link for map

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
     def show
         @product = Product.find_by_id(params[:id])
+        # 按购买人数排序，同时增加一些随机
         @hot_products = Product.find(:all, 
                                      :conditions => "status=#{PRODUCT_STATUS_ON_SHELF}",
                                      :order => "updated_at DESC",
