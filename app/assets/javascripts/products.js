efe2bbd2a983373662bd12ec90f5b40365c48acd
scige <panf06@gmail.com>
2012-12-08 23:56:03 +0800

    use jquery refector javascript code

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,26 @@
+$(function(){
+  var pos_x = $("#mapbox").attr("data-latitude");
+  var pos_y = $("#mapbox").attr("data-longitude");
+  
+  var map = new BMap.Map("mapbox");
+  var point = new BMap.Point(pos_x, pos_y);
+  
+  var marker1 = new BMap.Marker(new BMap.Point(pos_x, pos_y));
+  map.addOverlay(marker1);
+  
+  map.centerAndZoom(point,16);
+  //map.enableScrollWheelZoom();
+  map.addControl(new BMap.NavigationControl({type: BMAP_NAVIGATION_CONTROL_ZOOM}));
+  //map.addControl(new BMap.OverviewMapControl());
+  
+  $(".discountInfoRSide .button").click(function(){
+    $(".alert_mask").show(0);
+    $(".alert_out").show(0);
+  });
+  
+  $(".alert_out #cancel").click(function(){
+    $(".alert_mask").hide(0);
+    $(".alert_out").hide(0);
+  });
+});
+
