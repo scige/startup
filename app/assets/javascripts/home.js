dc56e6e71ca47af3ded65662997cc92b2f1e0fb0
scige <panf06@gmail.com>
2012-12-10 01:05:25 +0800

    add jquery.timers for couponmap auto close

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,19 @@
 $(function(){
-  $("#couponmap-title").toggle(function(){
-    $("#couponmap").slideUp(1000);
-    $("#couponmap-title #tips").html("点击这里可以<b>展开</b>优惠地图");
-  },function(){
-    $("#couponmap").slideDown(1000);
-    $("#couponmap-title #tips").html("点击这里可以<b>折叠</b>优惠地图");
+  $("#couponmap-title").click(function(){
+    if ($("#couponmap").is(":visible")){
+      $("#couponmap").slideUp(1000);
+      $("#couponmap-title #tips").html("点击这里可以<b>展开</b>优惠地图");
+    }else{
+      $("#couponmap").slideDown(1000);
+      $("#couponmap-title #tips").html("点击这里可以<b>折叠</b>优惠地图");
+    }
+  });
+
+  $("#couponmap-title").oneTime(5000, function(){
+    if ($("#couponmap").is(":visible")){
+      $("#couponmap").slideUp(1000);
+      $("#couponmap-title #tips").html("点击这里可以<b>展开</b>优惠地图");
+    }
   });
 
   $(".sysmsg .close").click(function(){
