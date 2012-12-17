a5c917e621dd42787094415f51b3b5bf29b9373b
scige <panf06@gmail.com>
2012-12-18 01:20:14 +0800

    use unique shop position to compute map center

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,13 +1,19 @@
 module HomeHelper
     def compute_map_center_x(products)
+        points = []
+        products.each {|p| points << [p.pos_x, p.pos_y]}
+        points.uniq!
         sum = 0
-        products.each {|p| sum += p.pos_x}
-        sum / products.size
+        points.each {|p| sum += p[0]}
+        sum / points.size
     end
 
     def compute_map_center_y(products)
+        points = []
+        products.each {|p| points << [p.pos_x, p.pos_y]}
+        points.uniq!
         sum = 0
-        products.each {|p| sum += p.pos_y}
-        sum / products.size
+        points.each {|p| sum += p[1]}
+        sum / points.size
     end
 end
