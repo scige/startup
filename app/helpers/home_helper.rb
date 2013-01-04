77e6d21fab3cccf8c1c8fa072b70667db843546e
scige <panf06@gmail.com>
2013-01-05 00:22:32 +0800

    refector couponmap center point

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,19 +1,15 @@
 module HomeHelper
     def compute_map_center_x(products)
         points = []
-        products.each {|p| points << [p.pos_x, p.pos_y]}
-        points.uniq!
-        sum = 0
-        points.each {|p| sum += p[0]}
-        sum / points.size
+        products.each {|p| points << p.pos_x}
+        points.sort!
+        (points[0] + points[-1]) / 2
     end
 
     def compute_map_center_y(products)
         points = []
-        products.each {|p| points << [p.pos_x, p.pos_y]}
-        points.uniq!
-        sum = 0
-        points.each {|p| sum += p[1]}
-        sum / points.size
+        products.each {|p| points << p.pos_y}
+        points.sort!
+        (points[0] + points[-1]) / 2
     end
 end
