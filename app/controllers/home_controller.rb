853906b4cbe837d6811346611991adb9995e4451
scige <panf06@gmail.com>
2013-04-18 02:14:52 +0800

    use random articles in homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,11 @@ class HomeController < ApplicationController
         @categories = Category.all
         @districts = District.all
         @products = Product.where(:status=>PRODUCT_STATUS_ON_SHELF).order("updated_at DESC").limit(6)
-        @articles = Article.where(:status=>ARTICLE_STATUS_ON).order("updated_at DESC").limit(8)
+        @articles = Article.find(:all,
+                                 :conditions => "status=#{ARTICLE_STATUS_ON}",
+                                 #:order => "RANDOM()",    #sqlite
+                                 :order => "rand()",       #mysql
+                                 :limit => 8)
     end
 
     def map
