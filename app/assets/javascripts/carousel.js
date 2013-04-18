c7e600f6216be2933dc1ef0452b16dda8286dc8b
scige <panf06@gmail.com>
2013-04-19 01:43:49 +0800

    modify products carousel

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,17 +1,17 @@
 jQuery(document).ready(function($) {
   $("#carousel-show").CloudCarousel({
     xPos: 480,
-    yPos: 120,
+    yPos: 100,
     xRadius:450,
-    yRadius:100,
+    yRadius:70,
     buttonLeft: $("#left-button"),
     buttonRight: $("#right-button"),
     titleBox: $('#da-vinci-title'),
     altBox: $('#da-vinci-alt'),
     reflHeight: 40,
-    reflGap:2,
-    speed:0.15,
-    autoRotate: 'right',
-    autoRotateDelay: 6000
+    reflGap:2
+    //speed:0.15,
+    //autoRotate: 'right',
+    //autoRotateDelay: 6000
   });
 });
