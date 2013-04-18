95d71622af268c1fb9cd65a1b9660e096baf156c
scige <panf06@gmail.com>
2013-04-19 02:12:28 +0800

    modify hot article css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
     def index
         @categories = Category.all
         @districts = District.all
-        @products = Product.where(:status=>PRODUCT_STATUS_ON_SHELF).order("updated_at DESC").limit(6)
+        @products = Product.where(:status=>PRODUCT_STATUS_ON_SHELF).order("updated_at DESC").limit(10)
         @articles = Article.find(:all,
                                  :conditions => "status=#{ARTICLE_STATUS_ON}",
                                  #:order => "RANDOM()",    #sqlite
